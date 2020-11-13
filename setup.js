1e3538706689c161731b471b6c45f8d89a210883
thepkd <dheeraj.putrevu@gmail.com>
2020-11-12 20:07:34 -0500

    Initial Commit

---
+++ setup.js
@@ -0,0 +1,123 @@
+var defaultEye = vec3.fromValues(0.5,0.5,-0.5); // default eye position in world space
+var defaultCenter = vec3.fromValues(0.5,0.5,0.5); // default view direction in world space
+var defaultUp = vec3.fromValues(0,1,0); // default view up vector
+var lightAmbient = vec3.fromValues(1,1,1); // default light ambient emission
+var lightDiffuse = vec3.fromValues(1,1,1); // default light diffuse emission
+var lightSpecular = vec3.fromValues(1,1,1); // default light specular emission
+var lightPosition = vec3.fromValues(-0.5,1.5,-0.5); // default light position
+var rotateTheta = Math.PI/50; // how much to rotate models by with each key press
+
+var Eye = vec3.clone(defaultEye); // eye position in world space
+var Center = vec3.clone(defaultCenter); // view direction in world space
+var Up = vec3.clone(defaultUp); // view up vector in world space
+
+function setupWebGL() {
+    
+    // Set up keys
+    ////document.onkeydown = handleKeyDown; // call this when key pressed
+
+
+    var imageCanvas = document.getElementById("myImageCanvas"); // create a 2d canvas
+      var cw = imageCanvas.width, ch = imageCanvas.height; 
+      imageContext = imageCanvas.getContext("2d"); 
+      var bkgdImage = new Image(); 
+      bkgdImage.crossorigin = "anonymous";
+      bkgdImage.src = "https://ncsucgclass.github.io/prog3/sky.jpg";
+      //bkgdImage.src = "https://raw.githubusercontent.com/thepkd/4x4bitFusion/master/564.png";
+      bkgdImage.onload = function(){
+          var iw = bkgdImage.width, ih = bkgdImage.height;
+          imageContext.drawImage(bkgdImage,0,0,iw,ih,0,0,cw,ch);   
+     }
+
+     
+    // Get the canvas and context
+    var canvas = document.getElementById("myWebGLCanvas"); // create a js canvas
+    gl = canvas.getContext("webgl"); // get a webgl object from it
+    
+    try {
+      if (gl == null) {
+        throw "unable to create gl context -- is your browser gl ready?";
+      } else {
+        //gl.clearColor(0.0, 0.0, 0.0, 1.0); // use black when we clear the frame buffer
+        gl.clearDepth(1.0); // use max when we clear the depth buffer
+        gl.enable(gl.DEPTH_TEST); // use hidden surface removal (with zbuffering)
+        //gl.enable(gl.BLEND);
+      }
+    } // end try
+    
+    catch(e) {
+      console.log(e);
+    } // end catch
+ 
+} // end setupWebGL
+
+
+// setup the webGL shaders
+function setupShaders() {
+    
+    // define vertex shader in essl using es6 template strings
+    var vShaderCode = `
+        attribute vec3 aVertexPosition; // vertex position
+        //uniform mat4 umMatrix; // the model matrix
+        uniform mat4 upvmMatrix; // the project view model matrix
+        
+        void main(void) {
+            
+            // vertex position
+            //vec4 vWorldPos4 = umMatrix * vec4(aVertexPosition, 1.0);
+            gl_Position = upvmMatrix * vec4(aVertexPosition, 1.0);
+
+        }
+    `;
+    
+    // define fragment shader in essl using es6 template strings
+    var fShaderCode = `
+        precision mediump float; // set float to medium precision
+            
+        void main(void) {
+        
+            // ambient term
+            gl_FragColor = vec4(1.0,1.0,1.0,1.0); 
+        }
+    `;
+    
+    try {
+        var fShader = gl.createShader(gl.FRAGMENT_SHADER); // create frag shader
+        gl.shaderSource(fShader,fShaderCode); // attach code to shader
+        gl.compileShader(fShader); // compile the code for gpu execution
+
+        var vShader = gl.createShader(gl.VERTEX_SHADER); // create vertex shader
+        gl.shaderSource(vShader,vShaderCode); // attach code to shader
+        gl.compileShader(vShader); // compile the code for gpu execution
+            
+        if (!gl.getShaderParameter(fShader, gl.COMPILE_STATUS)) { // bad frag shader compile
+            throw "error during fragment shader compile: " + gl.getShaderInfoLog(fShader);  
+            gl.deleteShader(fShader);
+        } else if (!gl.getShaderParameter(vShader, gl.COMPILE_STATUS)) { // bad vertex shader compile
+            throw "error during vertex shader compile: " + gl.getShaderInfoLog(vShader);  
+            gl.deleteShader(vShader);
+        } else { // no compile errors
+            var shaderProgram = gl.createProgram(); // create the single shader program
+            gl.attachShader(shaderProgram, fShader); // put frag shader in program
+            gl.attachShader(shaderProgram, vShader); // put vertex shader in program
+            gl.linkProgram(shaderProgram); // link program into gl context
+
+            if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) { // bad program link
+                throw "error during shader program linking: " + gl.getProgramInfoLog(shaderProgram);
+            } else { // no shader program link errors
+                gl.useProgram(shaderProgram); // activate shader program (frag and vert)
+                
+                // locate and enable vertex attributes
+                vPosAttribLoc = gl.getAttribLocation(shaderProgram, "aVertexPosition"); // ptr to vertex pos attrib
+                gl.enableVertexAttribArray(vPosAttribLoc); // connect attrib to array
+                
+                mMatrixULoc = gl.getUniformLocation(shaderProgram, "umMatrix"); // ptr to mmat
+                pvmMatrixULoc = gl.getUniformLocation(shaderProgram, "upvmMatrix"); // ptr to pvmmat
+            } // end if no shader program link errors
+        } // end if no compile errors
+    } // end try 
+    
+    catch(e) {
+        console.log(e);
+    } // end catch
+} // end setup shaders
