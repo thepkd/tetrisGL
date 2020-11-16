e84758489c90db1c34806890aa8ad789e0e6b12e
thepkd <dheeraj.putrevu@gmail.com>
2020-11-16 11:10:29 -0500

    Rotate Block

--- gameUtil.js
+++ gameUtil.js
@@ -1,3 +1,31 @@
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
+
+function rotateMatrix(block, dim) //Rotate a dimXdim matrix clockwise
+{
+  for(var i=0; i<dim; i++) //Transpose
+  {
+    for(let j=0; j<dim; j++)
+    {
+      if(j>=i)
+      {
+      let temp = block[i][j];
+      block[i][j] = block[j][i];
+      block[j][i] = temp;
+      }
+    }
+  }
+
+  //Reverse the rows
+  for(let i=0; i<dim; i++)
+  {
+    for(let j=0; j<dim/2; j++)
+    {
+      let temp = block[i][j];
+      block[i][j] = block[i][dim-1-j];
+      block[i][dim-1-j] = temp;
+    }
+  }
+
+}
